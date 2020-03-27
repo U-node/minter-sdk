f7c49436198401ae79d9b9575a97819af1cb2a82
rymka1989 <rymka1989@gmail.com>
2020-03-27 21:18:45 +0300

    base64 deeplink url

--- mintersdk/test/test_deeplink.py
+++ mintersdk/test/test_deeplink.py
@@ -14,27 +14,26 @@ class TestDeeplink(unittest.TestCase):
         deeplink = MinterDeeplink(tx=tx)
 
         self.assertEqual(
-            MinterDeeplink.BASE_URL + '?d=f84701aae98a424950000000000000009418467bbb64a8edf890201d526c35957d82be3d9588112210f4768db4008d436865636b207061796c6f616401018a4d4e5400000000000000',
+            MinterDeeplink.BASE_URL + '/-EcBqumKQklQAAAAAAAAAJQYRnu7ZKjt-JAgHVJsNZV9gr49lYgRIhD0do20AI1DaGVjayBwYXlsb2FkAQGKTU5UAAAAAAAAAA',
             deeplink.generate()
         )
 
-    def test_dataonly_with_payload(self):
+    def test_data_part_with_payload(self):
         tx = MinterSendCoinTx(
-            coin='BIP', to='Mx18467bbb64a8edf890201d526c35957d82be3d95',
-            value=1.23456789, nonce=1, gas_coin='MNT', gas_price=1,
-            payload='Hello World'
+            coin='MNT', to='Mx7633980c000139dd3bd24a3f54e06474fa941e16',
+            value=10, nonce=1, gas_coin='ASD', gas_price=1,
+            payload='custom message'
         )
         deeplink = MinterDeeplink(tx=tx)
         deeplink.nonce = ''
-        deeplink.gas_coin = ''
         deeplink.gas_price = ''
 
         self.assertEqual(
-            MinterDeeplink.BASE_URL + '?d=f83b01aae98a424950000000000000009418467bbb64a8edf890201d526c35957d82be3d9588112210f4768db4008b48656c6c6f20576f726c64808080',
+            MinterDeeplink.BASE_URL + '/-EgBqumKTU5UAAAAAAAAAJR2M5gMAAE53TvSSj9U4GR0-pQeFoiKxyMEiegAAI5jdXN0b20gbWVzc2FnZYCAikFTRAAAAAAAAAA',
             deeplink.generate()
         )
 
-    def test_dataonly(self):
+    def test_data_only(self):
         tx = MinterSendCoinTx(
             coin='BIP', to='Mx18467bbb64a8edf890201d526c35957d82be3d95',
             value=1.23456789, nonce=1, gas_coin='MNT', gas_price=1,
@@ -43,6 +42,6 @@ class TestDeeplink(unittest.TestCase):
         deeplink = MinterDeeplink(tx=tx, data_only=True)
 
         self.assertEqual(
-            MinterDeeplink.BASE_URL + '?d=f001aae98a424950000000000000009418467bbb64a8edf890201d526c35957d82be3d9588112210f4768db40080808080',
+            MinterDeeplink.BASE_URL + '/8AGq6YpCSVAAAAAAAAAAlBhGe7tkqO34kCAdUmw1lX2Cvj2ViBEiEPR2jbQAgICAgA',
             deeplink.generate()
         )
